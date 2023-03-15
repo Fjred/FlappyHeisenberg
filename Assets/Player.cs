eaff826122047221d6efd96f59a14f6953d5ff14
Fjred <karolis.aukstakojis@gmail.com>
2023-03-15 17:40:33 +0200

    jump

---
+++ Assets/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("jump");
+        }
+    }
+}
