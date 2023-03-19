eb118d446310b359cc98c853c7cfbda1352bcac7
Fjred <karolis.aukstakojis@gmail.com>
2023-03-19 17:16:22 +0200

    Random pipe coordinates and spawning

--- Assets/Pipe.cs
+++ Assets/Pipe.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Pipe : MonoBehaviour
 {
     public float speed;
+
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
     }
+
+    
 }
---
+++ Assets/Spawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float nextSpawnTime;
+    public float spawnInterval;
+
+
+    void Update()
+    {
+        if(Time.time > nextSpawnTime)
+        {
+            Spawn();
+            nextSpawnTime += spawnInterval;
+        }
+
+    }
+
+    void Spawn()
+    {
+        var pos = transform.position;
+        pos.y = Random.Range(-1f, 1f);
+        var rot = Quaternion.identity;
+
+        Instantiate(prefab, pos, rot);
+
+
+    }
+}
